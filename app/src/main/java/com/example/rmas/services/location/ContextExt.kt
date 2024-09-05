b6394568efda9a2f51596a6f6ffd32d8fbd8d487
Andjela Milicevic <andjela.milicevic@elfak.rs>
2024-09-05 12:38:32 +0200

    sitnica

--- app/src/main/java/com/example/rmas/services/location/ContextExt.kt
+++ app/src/main/java/com/example/rmas/services/location/ContextExt.kt
@@ -13,8 +13,5 @@ fun Context.hasLocationPermission(): Boolean {
             ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED &&
-            ContextCompat.checkSelfPermission(
-                this, Manifest.permission.POST_NOTIFICATIONS
             ) == PackageManager.PERMISSION_GRANTED
 }
